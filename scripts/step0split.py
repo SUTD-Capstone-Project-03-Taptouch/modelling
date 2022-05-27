b93e1ecf01d9940731e93c6f617dc0e33bf479e1
invertedplant <invertedplant@gmail.com>
2022-05-27 10:54:05 +0800

    Yeah ok it's cool
    It was the silence detection variable that was funky

--- scripts/step0split.py
+++ scripts/step0split.py
@@ -6,6 +6,8 @@ from pydub.silence import split_on_silence
 from pydub.utils import make_chunks
 from pydub.silence import detect_silence
 # TODO: Figure out what to do about ffmpeg being pesky
+# TODO: probably move the audio samples to their own "/audio" folder?
+# It seems like bad practice to have data in the scripts folder.
 
 # file locations for input and output audio
 RAWPATH = "rawaudio"
@@ -13,9 +15,9 @@ OUTPUTPATH = "splitaudio"
 
 # useful global variables
 DEFAULT_DURATION = 3000
-DEFAULT_FILETYPE = ".wav"
+# DEFAULT_FILETYPE = ".wav" yeah let's not
 SILENCE_DUR = 3000  # for how long should there be no sound before it's considered a silent segment
-SILENCE_THRESH = -16
+SILENCE_THRESH = -24  # this number is very finicky
 
 
 # def split_audio(duration, filetype):
@@ -33,10 +35,12 @@ def split_audio(duration):
                 print("Splitting complete!")
                 for segment in segments:
                     # no extra padding is done in this step since librosa can do it there
+                    # TODO: Test this on real conversational audio
+                    # TODO: Get the program to auto-delete files?
                     print("Making chunks!")
                     chunks = make_chunks(segment, duration)
                     for i, chunk in enumerate(chunks, start=count):
-                        chunk_name = OUTPUTPATH + "/chunk_{0}.wav".format(i)
+                        chunk_name = OUTPUTPATH + "/" + name + "_chunk_{0}.wav".format(i)
                         print("Exporting: " + chunk_name)
                         chunk.export(chunk_name, format="wav")  # this assumes the path exists
                         count = i + 1

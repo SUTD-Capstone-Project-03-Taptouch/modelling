deb98234a84f4a7093c2fc3ccb6fca0db93d49d4
invertedplant <invertedplant@gmail.com>
2022-06-08 13:15:02 +0800

    Added Librosa Processing
    Splitting it into librosa style format

--- scripts/step1split.py
+++ scripts/step1split.py
@@ -5,13 +5,12 @@ import ffmpeg
 from pydub import AudioSegment
 from pydub.silence import split_on_silence
 from pydub.utils import make_chunks
-from pydub.silence import detect_silence
 # TODO: Figure out what to do about ffmpeg being pesky
 # TODO: probably move the audio samples to their own "/audio" folder?
 # It seems like bad practice to have data in the scripts folder.
 
 # file locations for input and output audio
-RAW_PATH = "rawaudio"
+INPUT_PATH = "rawaudio"
 OUTPUT_PATH = "splitaudio"
 
 # useful global variables
@@ -23,14 +22,14 @@ SILENCE_THRESH = -24  # this number is very finicky
 
 # def split_audio(duration, filetype):
 def split_audio(duration):
-    for root, dirs, files in os.walk(RAW_PATH):
+    for root, dirs, files in os.walk(INPUT_PATH):
         start_time = time.time()
         for filename in files:
             count = 1
             name, extension = os.path.splitext(filename)
             print("Found " + filename)
             if extension == ".wav":
-                audio = AudioSegment.from_wav(RAW_PATH + "/" + filename)
+                audio = AudioSegment.from_wav(INPUT_PATH + "/" + filename)
                 print("Splitting for silence.")
                 segments = split_on_silence(audio, SILENCE_DUR, SILENCE_THRESH, keep_silence=500, seek_step=25)
                 print("Splitting complete!")
---
+++ scripts/step2process.py
@@ -0,0 +1,27 @@
+import librosa
+import numpy as np
+import os
+
+SAMPLE_RATE = 48000  # this is in kHz
+DURATION = 3
+OFFSET = 0.5
+INPUT_PATH = "splitaudio"
+# OUTPUT_PATH = "processedaudio"
+
+
+def librosafy():
+    signals = []
+    for root, dirs, files in os.walk(INPUT_PATH):
+        for i, file in enumerate(files):
+            audio, sample_rate = librosa.load(INPUT_PATH + "/" + file, duration=DURATION, offset=OFFSET, sr=SAMPLE_RATE)
+            signal = np.zeros((int(SAMPLE_RATE * 3, )))
+            signal[:len(audio)] = audio
+            signals.append(signal)
+            print("\r Processed {}/{} files.".format(i, len(files)), end=" ")
+    signals = np.stack(signals, axis=0)
+    print("\n", signals)
+    return signals
+
+
+if __name__ == "__main__":
+    librosafy()
